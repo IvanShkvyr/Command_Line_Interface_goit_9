f756a1dfa814ca51a2786daf25bbf2ab2fd8cb03
IvanShkvyr <Gis2011I@gmail.com>
2022-08-28 23:10:09 +0300

    Adds a birthday field and functionality. Adds pagination for AddressBook. Adds  functionality for checking the correctness of entered values ​​for the Phone and Birthday fields

--- README.md
+++ README.md
@@ -25,8 +25,39 @@ command "delete phone ..."
     Deletes all phones in the contact. Instead of ... the user enters a name.
     example: delate phone Ivan Shkvyr
 
+command "add email ..."
+    Adds e-mail to the contact. Instead of ... the user enters the name and e-mail, necessarily with a space.
+    example: add email Ivan Shkvyr gis2011i@gmail.com
+
+command "change email ..."
+    Changes e-mail to the entered one. Instead of ... the user enters the name and e-mail, necessarily with a space.
+    example: change email Ivan Shkvyr gis2011i@gmail.com
+
+command "delate email ..."
+    Deletes e-mail in the contact. Instead of ... the user enters a name.
+    example: delate email Ivan Shkvyr
+
+command "add birthday ..."
+    Adds birthday to the contact. Instead of ... the user enters the name and birthday (Numbers can be separated by any symbol), necessarily with a space.
+    example: add birthday Ivan Shkvyr 12-06-1987
+
+command "change birthday ..."
+    Changes birthday to the entered one. Instead of ... the user enters the name and birthday (Numbers can be separated by any symbol), necessarily with a space.
+    example: change birthday Ivan Shkvyr 19-06-1988
+
+command "delate birthday ..."
+    Deletes birthday in the contact. Instead of ... the user enters a name.
+    example: delate birthday Ivan Shkvyr
+
+command "when birthday ..."
+    Returns the number of days until the next birthday. Instead of ... the user enters a name.
+    example: when birthday Ivan Shkvyr
+
 "show all" command
     With this command, the bot outputs all saved contacts with phone numbers to the console.
+
+"show 3" command
+    Outputs 3 entries from the address book at a time
 
 command "good bye", "close", "exit"
     According to any of these commands, the bot completes its work after outputting "Good bye!" to the console.
--- all.py
+++ all.py
@@ -1,22 +1,33 @@
 from re import search
 from typing import Dict, Callable
 from collections import UserDict
+from datetime import datetime
 
 
 class Main:
 
+    # The data structure "is_phone_is_name"
+    # is_phone_is_name[command][0]==name, is_phone_is_name[command][1]==phone, is_phone_is_name[command][2]==email, is_phone_is_name[command][3]==birthday
     is_phone_is_name = {
-            "help": [False, False],
-            "hello": [False, False],
-            "create": [True, True],
-            "add phone": [True, True],
-            "change phone": [True, True],
-            "delate phone": [True, False],
-            "show all": [False, False],
-            "good bye": [False, False],
-            "close": [False, False],
-            "exit": [False, False]
-            } 
+                "help": [False, False, False, False],
+                "hello": [False, False, False, False],
+                "create": [True, True, False, False],
+                "add phone": [True, True, False, False],
+                "change phone": [True, True, False, False],
+                "delate phone": [True, False, False, False],
+                "add email": [True, False, True, False],
+                "change email": [True, False, True, False],
+                "delate email": [True, False, False, False],
+                "add birthday": [True, False, False, True],
+                "change birthday": [True, False, False, True],
+                "delate birthday": [True, False, False, False],
+                "when birthday": [True, False, False, False],
+                "show all": [False, False, False, False],
+                "show 3": [False, False, False, False],
+                "good bye": [False, False, False, False],
+                "close": [False, False, False, False],
+                "exit": [False, False, False, False]
+                } 
 
     def run_cli(self):
         while True:
@@ -24,7 +35,7 @@ class Main:
             user_input = input("Command: ")
 
             try:
-                command, record_name, phone_number = self.parse_user_input(user_input)
+                command, record_name, data_tail = self.parse_user_input(user_input)
             except Exception as e:
                 print(e)
                 continue
@@ -32,7 +43,7 @@ class Main:
             command_handler = handlers.get(command)
 
             try:
-                command_response = command_handler(record_name, phone_number)
+                command_response = command_handler(record_name, data_tail)
                 print(command_response)
             except SystemExit as e:
                 print(e)
@@ -41,10 +52,8 @@ class Main:
                 print(e)
                 continue
 
-
     def parse_user_input(self, user_input) -> set[str, str]:
-
-        command, is_record_name, is_phone_number = self.matching_command(user_input)
+        command, is_record_name, is_phone_number, is_email, is_birthday = self.matching_command(user_input)
 
         if is_phone_number:
             phone_number = self.matching_phone_number(user_input)
@@ -52,14 +61,35 @@ class Main:
             phone_number = None
             #By default, the phone number None is given for processing commands without a phone number
 
+        if is_email:
+            email = self.matching_email(user_input)
+        else:
+            email = None
+            #By default, the email None is given for processing commands without a email
+
+        if is_birthday:
+            birthday = self.matching_birthday(user_input)
+        else:
+            birthday = None
+            #By default, the birthday None is given for processing commands without a birthday
+
         if is_record_name:
-            record_name = self.matching_record_name(user_input, command, phone_number)
+            if is_email:
+                record_name = self.matching_record_name(user_input, command, email)
+            elif is_birthday:
+                record_name = self.matching_record_name(user_input, command, birthday)
+            else:
+                record_name = self.matching_record_name(user_input, command, phone_number)
         else:
             record_name = None
             #By default, the name None is given for processing commands without a name
-
-        return command, record_name, phone_number
-
+        
+        if is_email:
+            return command, record_name, email
+        elif is_birthday:
+            return command, record_name, birthday
+        else:
+            return command, record_name, phone_number
 
     def matching_command(self, user_input: str):
         """
@@ -68,9 +98,8 @@ class Main:
         full_command = user_input.lower()  # Converts the command to lower case
         for command in handlers:
             if command in full_command:
-                return command, self.is_phone_is_name[command][0], self.is_phone_is_name[command][1]
+                return command, self.is_phone_is_name[command][0], self.is_phone_is_name[command][1], self.is_phone_is_name[command][2], self.is_phone_is_name[command][3]
         raise ValueError("Unkown command!")
-
 
     def matching_phone_number(self, user_input): 
         """
@@ -81,30 +110,221 @@ class Main:
             raise AttributeError("Give me correct phone please")
         return phone.group()
 
+    def matching_email(self, user_input):
+        """
+        Searches for a e-mail in the entered pattern and returns the e-mail
+        """
+        email = search("\w+[.]?\w*[.]?\w*[.]?\w*\@\w{2,100}\.\w{2,100}",user_input)
+        if email == None:
+            raise AttributeError("Give me correct e-mail please")
+        return email.group()
 
-    def matching_record_name(self, user_input, command, phone_number):
+    def matching_birthday(self, user_input):
+        """
+        Searches for a birthday in the entered pattern and returns the birthday
+        """
+        birthday = search("\d\d.\d\d.\d\d\d\d",user_input)
+        if birthday == None:
+            raise AttributeError("Give me correct birthday please")
+        birthday = birthday.group()
+        if int(birthday[0:2]) > 31 or int(birthday[3:5]) > 12 or int(birthday[6:]) > 2022 or int(birthday[6:]) < 1900:
+            raise AttributeError("Give me correct birthday please")
+        return birthday
+
+    def matching_record_name(self, user_input, command, tail):
         """
         Searches for a name in the entered pattern and returns the name
         """
-        if phone_number == None:
-            phone_number = ""
-        record_name = user_input.replace(command, "").replace(phone_number, "").strip()
+        if tail == None:
+            tail = ""
+        record_name = user_input.replace(command, "").replace(tail, "").strip()
         if len(record_name) == 0:
             raise NameError('The name is incorrect') # Raises an error if the name is not specified
         return record_name
 
 
-def input_error(func):
-    def wrapper(*args, **kwargs):
+class Field:
+    def __init__(self, value: str):
+        self.__value = value
+
+    
+    @property
+    def value(self):
+        return self.__value
+
+
+    def __repr__(self):
+        return self.value
+
+
+class Name(Field):
+    
+    @Field.value.setter
+    def value(self, value):
+        self.__value = value
+
+
+class Phone(Field):
+    
+    @Field.value.setter
+    def value(self, value):
+        self.__value = value
+
+
+class Email(Field):
+    
+    @Field.value.setter
+    def value(self, value):
+        self.__value = value
+      
+
+class Birthday(Field):
+    
+    @Field.value.setter
+    def value(self, value):
+        self.__value = value
+
+
+class Record:
+
+    def __init__(self, name: Name, phone: Phone = None, email: Email = None, birthday: Birthday = None):
+        self.name: Name = name
+        self.phones: list[Phone] = [phone] if phone is not None else []
+        self.emails = email
+        self.birthdays = birthday
+
+    def __repr__(self):
+        return f"{self.name}, {' '.join(phone.value for phone in self.phones)}, {self.emails}, {self.birthdays}"
+       
+    def add_phone(self, name: Name, phone: Phone = None):
+        """Adds information about a new phone to the Record"""
+        name = Name(name)
+        phone = Phone(phone)
+        self.data.get(name.value, []).phones.append(phone)
+        return "The phone is recorded"
+
+    def change_phone(self, name: Name, phone: Phone = None):
+        """Changes all phone records to a new one"""
+        name = Name(name)
+        phone = Phone(phone)
+        self.data[name.value].phones.clear()
+        self.data[name.value].phones.append(phone)
+        return "The data has been changed"
+
+    def delate_phone(self, name: Name, phone: Phone = None):
+        """Removes phones"""
+        name = Name(name)
+        phone = Phone(phone)
         try:
-            result = func(*args, **kwargs)
-        except ValueError as e:
-            return ValueError
-        except KeyError as e:
-            return KeyError
+            self.data[name.value].phones.clear()
+            return "The phones has been delate" 
+        except ValueError:
+            return f"{phone} does not exist"
+
+    def add_email(self,name: Name, email: Email = None):
+        """Adds information about an email address to the Record"""
+        name = Name(name)
+        email = Email(email)
+        self.data.get(name.value, []).emails = email
+        return "The email is recorded"
+
+    def change_email(self,name: Name, email: Email = None):
+        """Changes email address"""
+        name = Name(name)
+        email = Email(email)
+        self.data[name.value].emails = email
+        return "The e-mail has been changed" 
+
+    def delate_email(self,name: Name, email: Email = None):
+        """Deletes an email address"""
+        name = Name(name)
+        self.data[name.value].emails = None
+        return "The e-mail has been delate" 
+
+    def add_birthday(self,name: Name, birthday: Birthday = None):
+        """Adds birthday information to the Record"""
+        name = Name(name)
+        birthday = birthday[0:2] + "_" + birthday[3:5] + "_" + birthday[6:]
+        birthday = Birthday(birthday)
+        self.data.get(name.value, []).birthdays = birthday
+        return "The birthday is recorded"
+
+    def change_birthday(self,name: Name, birthday: Birthday = None):
+        """Deletes the date of birth"""
+        name = Name(name)
+        birthday = birthday[0:2] + "_" + birthday[3:5] + "_" + birthday[6:]
+        birthday = Birthday(birthday)
+        self.data[name.value].birthdays = birthday
+        return "The birthday has been change" 
+
+    def delate_birthday(self,name: Name, birthday: Birthday = None):
+        """Deletes the date of birth"""
+        name = Name(name)
+        self.data[name.value].birthdays = None
+        return "The birthday has been delate" 
+
+    def days_to_birthday(self,name: Name, birthday: Birthday = None):
+        """Returns the number of days until the next birthday"""
+        name = Name(name)
+        current_date, current_year, next_year = current_date_and_year()
+
+        birthday = (datetime.strptime(str(self.data[name.value].birthdays), '%d_%m_%Y')).date()
+        birthday = birthday.replace(year=current_year)
+
+        if birthday < current_date:
+            result = (birthday.replace(year=next_year) - current_date).days
+        elif birthday > current_date:
+            result = (birthday - current_date).days
         else:
-            return result
-    return wrapper
+            return f"Today {name}'s birthday!!!"
+
+        return f" There are {result} days left until {name}'s birthday"
+
+
+class AddressBook(UserDict, Record):
+
+    MAX_VALUE_ON_PAGE = 3
+    count = 0
+
+    def __next__(self):
+        keys_list = list(self.data)
+        if self.count < len(keys_list) - self.MAX_VALUE_ON_PAGE:
+            element = ""
+            for n in range(self.MAX_VALUE_ON_PAGE):
+                element += f"{keys_list[self.count + n]}: {self.data[keys_list[self.count + n]]}\n" 
+            self.count += self.MAX_VALUE_ON_PAGE
+            return element
+        else:
+            tail = (len(keys_list) - self.count)
+            element = ""
+            for n in range(tail):
+                element += f"{keys_list[self.count + n]}: {self.data[keys_list[self.count + n]]}\n" 
+            print(element)
+            raise StopIteration("End of records")
+
+    def __iter__(self):
+        return self
+ 
+    def add_contact(self, name: Name, phone: Phone = None, *args):
+        """Adds a new contact to Record (Name and phone)"""
+        name = Name(name)
+        phone = Phone(phone)
+        contact = Record(name=name, phone=phone)
+        self.data[name.value] = contact
+        return "The data is recorded"
+
+    def show_all_h(self, *args):
+        """Returns all records"""
+        all_response = "Contacts book\n"
+        contacts = "\n".join(
+            f"{username} contacts is {number}" for (username, number) in address_book.data.items()
+            )
+        formated_contacts = "Number does not exists? yet!" if contacts == '' else contacts
+        return all_response + formated_contacts
+
+    def show_3_records(self, *args):
+        """Returns 3 records"""
+        return self.__next__()
 
 
 def help_h(*args):
@@ -121,140 +341,46 @@ def help_h(*args):
 def hello_h(*args):
     return "How can i help you?"
 
+
 def exit_h(*args):
     raise SystemExit("Good bye!!")
 
 
-class Field:
-    def __init__(self, value: str):
-        self.value = value
-
-
-    def __repr__(self):
-        return self.value
-
-
-class Name(Field):
-    pass
-
-
-class Phone(Field):
-    pass
-
-
-class Email(Field):
-    pass
-      
-
-class Record:
-
-    def __init__(self, name: Name, phone: Phone = None, email: Email = None):
-        self.name: Name = name
-        self.phones: list[Phone] = [phone] if phone is not None else []
-        self.email = email
-
-    def __repr__(self):
-        return f"{' '.join(phone.value for phone in self.phones)}, {self.email}"
-
-    
-    def add_phone(self, phone_number: Phone):
-        self.phones.append(phone_number)
-        
-    def change_phone(self, old_number: Phone , new_number: Phone ):
-        try:
-            self.phones.remove(old_number)
-            self.phones.append(new_number)
-        except ValueError:
-            return f"{old_number} does not exist"
-
-    def delete_phone(self, phone: Phone ):
-        try:
-            self.phones.remove(phone)
-        except ValueError:
-            return f"{phone} does not exist"
-
-
-class AddressBook(UserDict):
-
-    
-    def add_contact(self, name: Name, phone: Phone = None): ###
-        name = Name(name) ##############################
-        phone = Phone(phone) ###################################
-
-        # неможу викликати помилку
-        # for key in self.data.keys():
-        #     key = Name(key)
-        #     print(f"key == {key}, {type(key)}; name == {name}, {type(name)}")
-        #     if key == name:
-        #         raise ValueError('The name is duplicated') # Raises an error if the name is duplicated
-
-
-        contact = Record(name=name, phone=phone)
-        self.data[name.value] = contact
-
-        return "The data is recorded"
-
-
-
-
-    def add_phone_A(self, name: Name, phone: Phone = None):
-        name = Name(name) ##############################
-        phone = Phone(phone) ###################################
-
-        record_l = self.data.get(name.value, [])
-        record_l.phones.append(phone)
-        
-        return "The phone is recorded"
-
-    
-    def change_phone_A(self, name: Name, phone: Phone = None):
-        name = Name(name) ##############################
-        phone = Phone(phone) ###################################
-
-        record_l = self.data.get(name.value, [])
-        record_l.phones.clear()
-        record_l.phones.append(phone)
-
-        return "The data has been changed"
-
-
-    
-    def delete_phone_A(self, name: Name, *args):
-        name = Name(name) ##############################
-
-        record_l = self.data.get(name.value, [])
-        record_l.phones.clear()
-
-        return "The data has been delate"
-
-        
-
-    def show_all_h(self, *args):
-        all_response = "Contacts book\n"
-        contacts = "\n".join(
-            f"{username} contacts is {number}" for (username, number) in address_book.items()
-            )
-        formated_contacts = "Number does not exists? yet!" if contacts == '' else contacts
-        return all_response + formated_contacts
+def current_date_and_year():
+    """Returns the current date, the current year, and the next year"""
+    current_date = datetime.now().date()
+    current_year = current_date.year
+    next_year = current_year + 1
+    return current_date, current_year, next_year
 
 
 address_book = AddressBook() # Create an instance of the class AddressBook
 
-# Add metod add phone ----------------------------------
+
 handlers: Dict[str, Callable] = {
     "help": help_h,
     "hello": hello_h,
     "create": address_book.add_contact,
-    "add phone": address_book.add_phone_A,
-    "change phone": address_book.change_phone_A,
-    "delate phone": address_book.delete_phone_A,
+    "add phone": address_book.add_phone,
+    "change phone": address_book.change_phone,
+    "delate phone": address_book.delate_phone,
+    "add email": address_book.add_email,
+    "change email": address_book.change_email,
+    "delate email": address_book.delate_email,
+    "add birthday": address_book.add_birthday,
+    "change birthday": address_book.change_birthday,
+    "delate birthday": address_book.delate_birthday,
+    "when birthday": address_book.days_to_birthday,
     "show all": address_book.show_all_h,
+    "show 3": address_book.show_3_records,
     "good bye": exit_h,
     "close": exit_h,
     "exit": exit_h
     }
 
+
 if __name__ == "__main__":
+    phone_book = Main()  # Create an instance of the class
+    phone_book.run_cli()  # Run the main method
+
     
-    phone_book = Main()  # створюємо екземпляр класу
-    phone_book.run_cli()  # стартуємо головний метод
