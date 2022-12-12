d0ae5cc22a126ed2006fc1ec3a60e7ffac7425b1
IvanShkvyr <Gis2011I@gmail.com>
2022-12-12 20:24:37 +0200

    add LRU

---
+++ LRU.py
@@ -0,0 +1,17 @@
+import redis
+from redis_lru import RedisLRU
+
+client = redis.StrictRedis(host="localhost", port=6379, password=None)
+cache = RedisLRU(client)
+
+
+@cache
+def fibonacci(x):
+    if x in (0, 1):
+        return 1
+    return fibonacci(x-1) + fibonacci(x-2)
+
+
+if __name__ == "__main__":
+    for i in range(50):
+        print(f"fibonacci({i}) = {fibonacci(i)}")
